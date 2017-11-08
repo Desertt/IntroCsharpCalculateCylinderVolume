4061fc12c0fe59c6b68f88c08f615f19e06ce4aa
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2017-11-08 21:08:45 +0300

    IntroCsharpCalculateCylinderVolume - Silindirin Hacmini Hesaplayan Program/Calculate cylinder's volume

--- IntroCsharpCalculateCylinderVolume/Form1.Designer.cs
+++ IntroCsharpCalculateCylinderVolume/Form1.Designer.cs
@@ -28,12 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.textBoxRadius = new System.Windows.Forms.TextBox();
+            this.labelWarning = new System.Windows.Forms.Label();
+            this.buttonCount = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(6, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Yarıçap Giriniz/Enter radius:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelResult);
+            this.groupBox1.Controls.Add(this.textBoxRadius);
+            this.groupBox1.Controls.Add(this.labelWarning);
+            this.groupBox1.Controls.Add(this.buttonCount);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxHeight);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(593, 202);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Silindirin Hacmini Hesaplayan Program/Calculate cylinder\'s volume";
+            // 
+            // labelResult
+            // 
+            this.labelResult.Location = new System.Drawing.Point(163, 150);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(83, 24);
+            this.labelResult.TabIndex = 11;
+            this.labelResult.Text = "0";
+            // 
+            // textBoxRadius
+            // 
+            this.textBoxRadius.Location = new System.Drawing.Point(190, 26);
+            this.textBoxRadius.Name = "textBoxRadius";
+            this.textBoxRadius.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRadius.TabIndex = 2;
+            // 
+            // labelWarning
+            // 
+            this.labelWarning.Location = new System.Drawing.Point(6, 174);
+            this.labelWarning.Name = "labelWarning";
+            this.labelWarning.Size = new System.Drawing.Size(427, 17);
+            this.labelWarning.TabIndex = 10;
+            this.labelWarning.Text = "Henüz Hesaplanmadı/Not yet calculated";
+            this.labelWarning.Visible = false;
+            // 
+            // buttonCount
+            // 
+            this.buttonCount.Location = new System.Drawing.Point(192, 101);
+            this.buttonCount.Name = "buttonCount";
+            this.buttonCount.Size = new System.Drawing.Size(98, 30);
+            this.buttonCount.TabIndex = 6;
+            this.buttonCount.Text = "Hesapla/Count";
+            this.buttonCount.UseVisualStyleBackColor = true;
+            this.buttonCount.Click += new System.EventHandler(this.buttonCount_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(6, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(159, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Yükseklik Giriniz/Height Enter:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(9, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Silindirin Hacmi/Cylinder Volume:";
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(190, 63);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHeight.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(593, 202);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Silindirin Hacmini Hesaplayan Program/Calculate cylinder\'s volume";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.TextBox textBoxRadius;
+        private System.Windows.Forms.Label labelWarning;
+        private System.Windows.Forms.Button buttonCount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxHeight;
     }
 }
 
--- IntroCsharpCalculateCylinderVolume/Form1.cs
+++ IntroCsharpCalculateCylinderVolume/Form1.cs
@@ -16,5 +16,41 @@ namespace IntroCsharpCalculateCylinderVolume
         {
             InitializeComponent();
         }
+
+        #region Variables
+        public double cylinderHeight;
+        public double cylinderRadius;
+        public double cylinderVolume;
+        public const double piValue = Math.PI;
+       
+        #endregion
+
+        private void buttonCount_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBoxRadius.Text))
+            {
+                labelWarning.Visible = true;
+                labelWarning.Text = "Silindirin Yarıçapını Giriniz ! / Please Enter Radius of Cylinder";
+                labelWarning.BackColor = Color.Red;
+                return;
+            }
+            else if (String.IsNullOrEmpty(textBoxHeight.Text))
+            {
+                labelWarning.Visible = true;
+                labelWarning.Text = "Silindirin Yüksekliğini Giriniz ! / Please Enter Height of Cylinder";
+                labelWarning.BackColor = Color.Red;
+                return;
+            }
+            else
+            {
+                cylinderHeight = Convert.ToDouble(textBoxHeight.Text);
+                cylinderRadius = Convert.ToDouble(textBoxRadius.Text);
+                cylinderVolume = Math.PI * Math.Pow(cylinderRadius, 2) * cylinderHeight;
+                labelResult.Text = cylinderVolume.ToString();
+                labelWarning.Visible = true;
+                labelWarning.Text = "Hesaplama Başarılı :)";
+                labelWarning.BackColor = Color.Turquoise;
+            }
+        }
     }
 }
